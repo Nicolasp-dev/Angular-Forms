086b62c938cd9c7e2a09f7321fd315ce8764f6e4
Nicolas Peña <103202106+Nicolasp-dev@users.noreply.github.com>
2023-09-18 20:30:13 -0500

    Feat: Form Array Examplel

--- src/app/create-course/create-course-step-3/create-course-step-3.component.ts
+++ src/app/create-course/create-course-step-3/create-course-step-3.component.ts
@@ -6,4 +6,27 @@ import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
   templateUrl: "create-course-step-3.component.html",
   styleUrls: ["create-course-step-3.component.scss"],
 })
-export class CreateCourseStep3Component {}
+export class CreateCourseStep3Component {
+  form = this.fb.group({
+    lessons: this.fb.array([]),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  get lessons() {
+    return this.form.controls["lessons"] as FormArray;
+  }
+
+  addLesson() {
+    const lessonForm = this.fb.group({
+      title: ["", Validators.required],
+      level: ["beginner", Validators.required],
+    });
+
+    this.lessons.push(lessonForm);
+  }
+
+  deleteLesson(i: number) {
+    this.lessons.removeAt(i);
+  }
+}
